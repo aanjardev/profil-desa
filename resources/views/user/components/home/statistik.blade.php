2b468fbc36d784d7bad81b59cc4cac22fb112454
aanjardev <ailapyuu3000@gmail.com>
2026-07-09 14:44:04 +0700

    fix: responsive section hero

--- resources/views/user/components/home/statistik.blade.php
+++ resources/views/user/components/home/statistik.blade.php
@@ -5,20 +5,20 @@
     <div class="g-padding-y-80--xs g-padding-y-125--sm" style="position: relative; z-index: 2;">
         <div class="container g-text-center--xs">
             <div class="row">
-                <div class="col-sm-4 g-margin-b-60--xs g-margin-b-0--md">
-                    <i class="ti-map-alt g-font-size-50--xs g-color--white g-margin-b-20--xs"></i>
-                    <div class="g-font-size-40--xs g-font-weight--700 g-color--white g-margin-b-10--xs">{{ $statistics['luas_wilayah'] }}</div>
-                    <div class="text-uppercase g-font-size-14--xs g-color--white-opacity">Luas Wilayah (Ha)</div>
+                <div class="col-xs-4 col-sm-4">
+                    <i class="ti-map-alt g-font-size-24--xs g-font-size-50--md g-color--white g-margin-b-10--xs g-margin-b-20--md"></i>
+                    <div class="g-font-size-18--xs g-font-size-40--md g-font-weight--700 g-color--white g-margin-b-5--xs g-margin-b-10--md">{{ $statistics['luas_wilayah'] }}</div>
+                    <div class="text-uppercase g-font-size-10--xs g-font-size-14--md g-color--white-opacity">Luas Wilayah (Ha)</div>
                 </div>
-                <div class="col-sm-4 g-margin-b-60--xs g-margin-b-0--md">
-                    <i class="ti-user g-font-size-50--xs g-color--white g-margin-b-20--xs"></i>
-                    <div class="g-font-size-40--xs g-font-weight--700 g-color--white g-margin-b-10--xs">{{ $statistics['jumlah_penduduk'] }}</div>
-                    <div class="text-uppercase g-font-size-14--xs g-color--white-opacity">Jumlah Penduduk</div>
+                <div class="col-xs-4 col-sm-4">
+                    <i class="ti-user g-font-size-24--xs g-font-size-50--md g-color--white g-margin-b-10--xs g-margin-b-20--md"></i>
+                    <div class="g-font-size-18--xs g-font-size-40--md g-font-weight--700 g-color--white g-margin-b-5--xs g-margin-b-10--md">{{ $statistics['jumlah_penduduk'] }}</div>
+                    <div class="text-uppercase g-font-size-10--xs g-font-size-14--md g-color--white-opacity">Jumlah Penduduk</div>
                 </div>
-                <div class="col-sm-4">
-                    <i class="ti-home g-font-size-50--xs g-color--white g-margin-b-20--xs"></i>
-                    <div class="g-font-size-40--xs g-font-weight--700 g-color--white g-margin-b-10--xs">{{ $statistics['jumlah_rt_rw'] }}</div>
-                    <div class="text-uppercase g-font-size-14--xs g-color--white-opacity">RT / RW</div>
+                <div class="col-xs-4 col-sm-4">
+                    <i class="ti-home g-font-size-24--xs g-font-size-50--md g-color--white g-margin-b-10--xs g-margin-b-20--md"></i>
+                    <div class="g-font-size-18--xs g-font-size-40--md g-font-weight--700 g-color--white g-margin-b-5--xs g-margin-b-10--md">{{ $statistics['jumlah_rt_rw'] }}</div>
+                    <div class="text-uppercase g-font-size-10--xs g-font-size-14--md g-color--white-opacity">RT / RW</div>
                 </div>
             </div>
         </div>
