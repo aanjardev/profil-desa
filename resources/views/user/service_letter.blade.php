dcce60f8bc606bd3b72f6a8f636628aaf3397c4f
aanjardev <ailapyuu3000@gmail.com>
2026-07-15 17:28:17 +0700

    fix: nav func + responsivitas layanan surat

--- resources/views/user/service_letter.blade.php
+++ resources/views/user/service_letter.blade.php
@@ -2,6 +2,8 @@
 @section('content')
 <!--========== PAGE CONTENT ==========-->
 @include('user.components.services_letter.page_header_services_letter')
-@include('user.components.services_letter.services_letter_list')        
+<div class="g-bg-color--sky-light">
+    @include('user.components.services_letter.services_letter_list')        
+</div>
 <!--========== END PAGE CONTENT ==========-->
 @endsection
